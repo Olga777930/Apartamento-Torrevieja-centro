382cb4198283af63750c043e7f279fb35b3ee7e2
Olga777930 <ok748681@gmail.com>
2025-06-03 16:22:32 +0200

    360

---
+++ data.js
@@ -0,0 +1,677 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-la-callejpg",
+      "name": "entrada la callejpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.657632561127812,
+        "pitch": -0.04554053197778529,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1532586771861606,
+          "pitch": 0.007475589457275333,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": -2.50107042577579,
+          "pitch": -0.1984532613549792,
+          "rotation": 0,
+          "target": "12-supermercado"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada",
+      "name": "entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.977172697325406,
+        "pitch": -0.08528313390425701,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0022325200922139743,
+          "pitch": 0.0774204707574917,
+          "rotation": 0,
+          "target": "0-entrada-la-callejpg"
+        },
+        {
+          "yaw": -3.029689271885374,
+          "pitch": 0.029209437481402034,
+          "rotation": 0,
+          "target": "2-corredor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-corredor",
+      "name": "corredor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.22349871309014446,
+        "pitch": 0.0014489024908410642,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1209445965638967,
+          "pitch": 0.07700205917127079,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": 0.12283137931742516,
+          "pitch": -0.008607964544438573,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-cocina",
+      "name": "cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.6159994099419297,
+        "pitch": 0.0731053912766928,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1369625368301977,
+          "pitch": 0.14891190621356643,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-salon",
+      "name": "salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.9970300861080759,
+        "pitch": -0.12168427373200608,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7492170471471695,
+          "pitch": 0.033232159509150705,
+          "rotation": 0,
+          "target": "3-cocina"
+        },
+        {
+          "yaw": -1.2635576096055878,
+          "pitch": 0.07348920560964345,
+          "rotation": 0,
+          "target": "2-corredor"
+        },
+        {
+          "yaw": 2.937510788484813,
+          "pitch": 0.18746269605077082,
+          "rotation": 0,
+          "target": "5-bano"
+        },
+        {
+          "yaw": -2.927128581592463,
+          "pitch": 0.13377220287980407,
+          "rotation": 0,
+          "target": "6-dormitorio"
+        },
+        {
+          "yaw": 2.594059499711915,
+          "pitch": 0.06188350498254813,
+          "rotation": 0,
+          "target": "7-dormitorio-2"
+        },
+        {
+          "yaw": -0.3366714149539103,
+          "pitch": 0.019762771263120982,
+          "rotation": 0,
+          "target": "9-dormitorio-3"
+        },
+        {
+          "yaw": 1.0020617174476296,
+          "pitch": -0.03102401490712481,
+          "rotation": 0,
+          "target": "10-balcon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bano",
+      "name": "baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.642879044985051,
+        "pitch": 0.022945592866911113,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.766051038537551,
+          "pitch": 0.032796046960145375,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dormitorio",
+      "name": "dormitorio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.7098937770132991,
+        "pitch": -0.029657399603106427,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.032965221983437,
+          "pitch": 0.0862838097761589,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dormitorio-2",
+      "name": "dormitorio 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.6989862818214476,
+        "pitch": -0.011899972172702888,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1184321851402572,
+          "pitch": 0.05959919408066483,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bano-2",
+      "name": "baño 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.646992102179766,
+        "pitch": 0.06346651825434435,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.646992102179766,
+          "pitch": 0.06346651825434435,
+          "rotation": 0,
+          "target": "4-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-dormitorio-3",
+      "name": "dormitorio 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.6753909765509105,
+        "pitch": -0.020013809078072597,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8948298423910401,
+          "pitch": 0.05285231731027018,
+          "rotation": 0,
+          "target": "8-bano-2"
+        },
+        {
+          "yaw": -2.407075158272569,
+          "pitch": 0.04520487832425957,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": 1.9222294087125258,
+          "pitch": 0.09516448960674673,
+          "rotation": 0,
+          "target": "10-balcon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-balcon",
+      "name": "balcon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.5659205340252527,
+        "pitch": 0.11162082152871022,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09228522056791455,
+          "pitch": 0.2621952662655822,
+          "rotation": 0,
+          "target": "9-dormitorio-3"
+        },
+        {
+          "yaw": -1.1356469489535908,
+          "pitch": 0.043317364724668295,
+          "rotation": 0,
+          "target": "4-salon"
+        },
+        {
+          "yaw": -2.1728371030737375,
+          "pitch": 0.11251966226205035,
+          "rotation": 0,
+          "target": "12-supermercado"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-la-calle",
+      "name": "la calle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.9808143134316687,
+        "pitch": -0.21618282780383424,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8668848470421953,
+          "pitch": -0.03852401743636413,
+          "rotation": 0,
+          "target": "0-entrada-la-callejpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-supermercado",
+      "name": "supermercado",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.39132475255479093,
+        "pitch": 0.07912455451932843,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8764264502894719,
+          "pitch": -0.03230879358828531,
+          "rotation": 0,
+          "target": "13-la-playa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-la-playa",
+      "name": "la playa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.730478059475459,
+        "pitch": -0.019631982335846843,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8973206958530504,
+          "pitch": -0.06360558143770234,
+          "rotation": 0,
+          "target": "12-supermercado"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
